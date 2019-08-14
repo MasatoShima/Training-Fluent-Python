63392915ca993acb748cc2c13a3893e2e43321aa
MasatoShima <masato.shima@icloud.com>
2019-08-15 03:08:03 +0900

    FrenchDeck Class の操作例を追記

--- Pythonic_Trump.py
+++ Pythonic_Trump.py
@@ -1,3 +1,4 @@
+# %%
 """
 Name: Masato Shima
 Description: Pythonic trump
@@ -9,6 +10,7 @@ Created on: 2019/08/12
 # ----- Import Library
 # **************************************************
 import collections
+from random import choice
 
 
 # **************************************************
@@ -38,18 +40,39 @@ class FrenchDeck:
 		return self.cards[position]
 
 
-# **************************************************
-# ----- Function Main
-# **************************************************
+# %%
+beer_card = Card(8, "spades")
+
+print(beer_card)
+print(beer_card.rank)
+print(beer_card.suit)
 
 
-# **************************************************
-# ----- Process Main
-# **************************************************
-if __name__ == '__main__':
-	pass
+# %%
+deck = FrenchDeck()
+
+# __len__ により要素の数を取得可能に
+print(deck)
+print(len(deck))
+
+# __getitem__ により FrenchDeck Class は Iterable なデータ型として扱える
+# Slicing
+print(deck[0])
+print(deck[-1])
+
+# random ライブラリよりランダムな要素の取得も行える
+print(choice(deck))
+
+# loop による要素の取得も可能に
+for card in deck:
+	print(card)
+
+# 要素の順の反転も可能
+for card in reversed(deck):
+	print(card)
 
 
+# %%
 # **************************************************
 # ----- End
 # **************************************************
